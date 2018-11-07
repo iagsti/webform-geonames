b4beecc0aaf2261e6bac2b7e8959d566860557d2
Marcelo Schneider <schneider.fei@gmail.com>
2018-11-07 14:29:53 -0200

    Requesting geonames API - issues #21
    Modifying Geonames library in order to request the Geonames webservice

---
+++ src/libraries/Geonames.php
@@ -0,0 +1,107 @@
+<?php 
+
+namespace Drupal\webform_geonames\libraries;
+
+use GuzzleHttp\Exception\RequestException;
+
+const API = 'http://api.geonames.org/';
+
+class Geonames {
+  
+  private $geonamesLogin;
+  private $api;
+
+  public function __construct($geonamesLogin)
+  {
+    $this->geonamesLogin = $geonamesLogin;
+    $this->api = API;
+  }
+
+  public function getCountryList() {
+
+    $resource = $this->makeResource(null, 'countryInfoJSON');
+    $response =  $this->makeRequest($resource);
+    $options = $this->generateOptions('countryName', $response->geonames);
+
+    return $options;
+
+  }
+
+  public function getStateListByCountryName($countryCode) {
+
+    $resource = $this->makeResource(null, 'countryInfoJSON');
+    $resource .= '&country=' . $countryCode;
+    $country = $this->makeRequest($resource);
+    $countryGeonameId = $this->getGeonameId($country);
+
+    $stateOptions = $this->getStateList($countryGeonameId);
+    
+    return $stateOptions;
+
+  }
+
+  public function getStateList($countryGeonameId) {
+
+    $resource = $this->makeResource($countryGeonameId, 'childrenJSON');
+    $response = $this->makeRequest($resource);
+
+    $options = $this->generateOptions('toponymName', $response->geonames);
+
+    return $options;
+
+  }
+
+  public function getCityList($stateGeonameId) {
+
+    $resource = $this->makeResource($stateGeonameId, 'childrenJSON');
+    $response = $this->makeRequest($resource);
+    $options = $this->generateOptions('toponymName', $response->geonames);
+
+    return $options;
+
+  }
+
+  public function makeRequest ($resource) {
+    
+    $client = \Drupal::httpClient();
+    $response = [];
+
+    try {
+      $request = $client->get($resource);
+      $response = $request->getBody();
+    } catch (RequestException $e) {
+      drupal_set_message($e);
+    }
+
+    return json_decode($response);
+  }
+
+  public function makeResource ($geonameId, $resource) {
+    
+    $geonameIdParameter = $geonameId ? 'geonameId=' . $geonameId : '';
+    $loginParameter = $this->geonamesLogin ? '&username=' . $this->geonamesLogin : '';
+    $query = '?';
+
+    $request = $this->api . $resource . $query . $geonameIdParameter . $loginParameter;
+
+    return $request;
+
+  }
+
+  protected function generateOptions($key, array $data) {
+
+    $options = array_map(function($item) use ($key) {
+      return $item->{$key};
+    }, $data);
+
+    return array_combine($options, $options);
+
+  }
+
+  protected function getGeonameId($geonames) {
+
+    return $geonames->geonames[0]->geonameId;
+
+  }
+
+}
